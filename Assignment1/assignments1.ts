950c7dfbcddf113d7702b6e6d8e1ae55a3b7722e
dipika sasamal <dpka.mails@gmail.com>
2026-04-17 21:07:25 +0530

    assignment 1

---
+++ Assignment1/assignments1.ts
@@ -0,0 +1,65 @@
+//Temperature of a city in degrees Celsius: 25.5
+let temperature: number = 25.5;
+console.log(`1.Temperature of a city in degrees Celsius: ${temperature}c`);
+//Whether a customer has placed an order: true or false
+let order: boolean =true;
+console.log(`2.Whether a customer has placed an order: ${order}`);
+//Person's phone number: "123-456-7890"
+let phone_numb: number =123456789;
+console.log(`3.Person's phone number: ${phone_numb}`)
+//Amount of money in a customer's bank account: 1000.50
+let bank_acc: number = 1000.50;
+console.log(`4.Amount of money in a customer's bank account: ${bank_acc}`)
+//Person's email address: "john.doe@example.com"
+let email_id: string ="john.doe@example.com";
+console.log(`5.Person's email address: "${email_id}"`)
+
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let location1: {latitude: number; longitude: number} ={latitude: 37.7749, longitude:-122.4194};
+console.log(`6.Coordinates of a location (latitude, longitude): ${location1}`)
+
+//Person's marital status: true or false
+let marital_status: boolean =true;
+console.log(`7.Person's marital status: ${marital_status}`);
+
+//Person's occupation: "Software Engineer"
+let occupation: string ="Software Engineer"
+console.log(`8.Person's occupation: "${occupation}"`)
+// Person's favourite colour: "Blue"
+let fav_color: string ="Blue"
+console.log(`9.Person's occupation: "${fav_color}"`)
+//Current year: 2023
+let year: number = 2023;
+console.log (` 10.Current year: ${year}`)
+//Number of followers on a social media platform: 1,000,000
+let follower: number = 1000000
+console.log(`11.Number of followers on a social media platform: ${follower}`)
+//Rating of a movie: 7.5
+let movie: number = 7.5
+console.log(`12.Rating of a movie: ${movie}`)
+//Person's blood type: 'A'
+let blood_type: string ="A"
+console.log(`13.Person's blood type:: '${blood_type}'`)
+
+//Title of a book: "To Kill a Mockingbird"
+let Title_book: string ="To Kill a Mockingbird"
+console.log(`14.Title of a book:: "${Title_book}"`)
+//Number of employees in a company: 500
+let emp_count: number = 500
+console.log(`15.Number of employees in a company: ${emp_count}`)
+//Time of an event: 2:30 PM
+let Time_event: string ="2:30 pm"
+console.log(`16.Name of a country:: ${Time_event}`)
+//Name of a country: "United States"
+let country: string ="United States"
+console.log(`17.Name of a country:: "${country}"`)
+
+//Person's eye color: "Brown"
+let eye_col: string ="Brown"
+console.log(`18.Person's eye color:  "${eye_col}"`)
+//Person's birthplace: "New York City"
+let birth_place: string ="New York City"
+console.log(`19.Person's birthplace: "${country}"`)
+// Distance between two cities: 200.5
+let cities:number = 200.5
+console.log(`20.Distance between two cities: ${cities}`)
